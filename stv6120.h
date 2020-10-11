e530d6d333afbbcffb2e81cec03f36160ea6753c
Phil Crump <phil@philcrump.co.uk>
2020-10-11 18:09:37 +0100

    Power down both stv6120 signal paths between tuner lock retries.

--- stv6120.h
+++ stv6120.h
@@ -44,6 +44,7 @@ uint8_t stv6120_init(uint32_t, uint32_t, bool);
 uint8_t stv6120_set_freq(uint8_t, uint32_t);
 uint8_t stv6120_cal_lowpass(uint8_t);
 void stv6120_print_settings();
+uint8_t stv6120_powerdown_both_paths(void);
 
 #endif
 
